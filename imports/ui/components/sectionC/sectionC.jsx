746ad338ffb7465bc58f131d9dbd9b79e9dbb4c9
Ivan <ivan.baena04@gmail.com>
2017-02-02 00:55:50 -0500

    sleep

--- imports/ui/components/sectionC/sectionC.jsx
+++ imports/ui/components/sectionC/sectionC.jsx
@@ -26,6 +26,7 @@ export default class SectionC extends Component {
           <div className="col-md-12" id="sectionC-content">
             <h2 className="text-center section-title">Expecto Patronum</h2>
             <hr id="sectionC-break" />
+
             <p className="text-center">
               Red hair crookshanks bludger Marauder’s Map Prongs sunshine daisies butter mellow Ludo Bagman. Beaters gobbledegook N.E.W.T., Honeydukes eriseD inferi Wormtail. Mistletoe dungeons Parseltongue Eeylops Owl Emporium expecto patronum floo powder duel. Gillyweed portkey, keeper Godric’s Hollow telescope, splinched fire-whisky silver Leprechaun O.W.L. stroke the spine. Chalice Hungarian Horntail, catherine wheels Essence of Dittany Gringotts Harry Potter. Prophecies Yaxley green eyes Remembrall horcrux hand of the servant. Devil’s snare love potion Ravenclaw, Professor Sinistra time-turner steak and kidney pie. Cabbage Daily Prophet letters from no one Dervish and Banges leg.
             </p>
